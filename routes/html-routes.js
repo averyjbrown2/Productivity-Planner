bf50c7195d587f08c48040dc20d0386d4cf15296
Lindsay Morris <lindsaymorris813@gmail.com>
2020-12-17 11:45:03 -0500

    html routing

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/dashboard");
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
@@ -16,14 +16,30 @@ module.exports = function(app) {
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/dashboard");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/dashboard", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/dashboard.html"));
+  });
+
+  app.get("/community", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/community.html"));
+  });
+
+  app.get("/goals", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/goals.html"));
+  });
+
+  app.get("/dayplanner", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "..public/dayplanner.html"));
+  });
+
+  app.get("/signup", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "..public/signup.html"));
   });
 };
