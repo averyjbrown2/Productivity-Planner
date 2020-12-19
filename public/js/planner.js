aca9909108387d6af2a3dc92078c887bb1ea1863
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-12-18 22:39:53 -0500

    changed functions to look for matching date ID first. Then upload notes associated with that Id

--- public/js/planner.js
+++ public/js/planner.js
@@ -1,7 +1,14 @@
+
 $(document).ready(() => {
   // Getting a reference to the input field where user adds a new todo
   const newNoteInput = $("#new-note");
+
   $("#note-save-btn").on("click", saveNote);
+
+  const dateID = window.location.href.substring(
+    window.location.href.lastIndexOf("/") + 1
+  );
+
 
   //Run these functions when the planner page loads
   getNote();
@@ -10,6 +17,10 @@ $(document).ready(() => {
   function getNote() {
     $.get("/api/notes", data => {
       note = data;
+      if (note.length === 0) {
+        console.log("NO DATA SO WE QUIT FUNCTION");
+        return;
+      }
       renderNotes();
     });
   }
@@ -17,28 +28,44 @@ $(document).ready(() => {
   //upload Notes onto page
   function renderNotes() {
     newNoteInput.empty();
-
-    //need to change this to a loop that goes through the notes data base and only popluates the note where the date matches the date the user is on
-    newNoteInput.val(note[0].text);
-    console.log(note);
+    console.log("RENDER NOTES HAS RUN!");
+    for (let i = 0; i < note.length; i++) {
+      if (dateID === note[i].date) {
+        console.log(note[i]);
+        newNoteInput.val(note[i].text);
+        console.log(note);
+      }
+    }
   }
 
   function saveNote() {
+    console.log("CLICKED!");
     //grab notes table
     $.get("/api/notes", data => {
       note = data;
-
-      //goes to the first row of the table
-      const savedNote = note[0];
-
-      //changes the value of the exisitng not text to the value of the textarea
-      savedNote.text = newNoteInput.val();
-      //runs update note function
-      updateNote(savedNote);
     });
+    if (note.length === 0) {
+      insertNote();
+    } else {
+      for (let i = 0; i < note.length; i++) {
+        if (dateID === note[i].date) {
+          const savedNote = note[i];
+
+          console.log("FOUND A MATCH!!: Date - " + savedNote.dateId + " Text - " + savedNote.Id );
+          savedNote.text = newNoteInput.val();
+          console.log("Updated Note!: Date - " + savedNote.dateId + " Text - " + savedNote.Id );
+          //runs update note function
+          updateNote(savedNote);
+        } else {
+          console.log("NO matching date so INSERTING NEW NOTE INTO TABLE");
+          insertNote();
+        }
+      }
+    }
   }
 
   function updateNote(note) {
+    console.log("UPDATE NOTE HAS RUN");
     //uses put method the take in saved not and runs route using saved note
     $.ajax({
       method: "PUT",
@@ -47,12 +74,13 @@ $(document).ready(() => {
     }).then(getNote);
   }
 
-  // function insertNote(event) {
-  //   event.preventDefault();
-  //   console.log(newNoteInput.val());
-  //   var note = {
-  //     text: newNoteInput.val().trim(),
-  //   };
-  //   $.post("/api/notes", note);
-  // }
+  function insertNote() {
+    console.log("INSERT NOTE RUN");
+    const note = {
+      date: dateID,
+      text: newNoteInput.val().trim()
+    };
+    console.log(note);
+    $.post("/api/notes", note);
+  }
 });
